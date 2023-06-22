5c1975dbbb4067e6dcbf0e225a80f1b06fae48be
JoaksCodes <125510235+JoaksCodes@users.noreply.github.com>
2023-06-22 16:05:28 -0300

    Corregi error let

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const edad = prompt("DNI en mano!");
+let edad = prompt("DNI en mano!");
 
 if (edad > 60) {
   console.log("Lo siento, estás muy viejo, vay a mimir.");
 } else if (edad >= 18) {
   console.log("Pase!");
 } else if (edad >= 16) {
-  const tieneTutor = prompt("Viniste con un tutor pichon/a? (responde sí o no)").toLowerCase();
+  let tieneTutor = prompt("Viniste con un tutor pichon/a? (responde si o no)").toLowerCase();
 
-  if (tieneTutor === "sí") {
+  if (tieneTutor === "si") {
     console.log("Puedes pasar con tu tutor pichon");
   } else {
     console.log("Solo/a, ni a gancho!");
   }
 } else {
-  console.log("So muy chiquito, vaya jugar minecraft");
+  console.log("So muy chiquito, vaya jugar minecraft!");
 }
